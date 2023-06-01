f39eca0469abe94ae96834c9d1a6f9cee34111fc
HAlexis88 <126806311+HAlexis88@users.noreply.github.com>
2023-06-01 00:28:04 -0700

    Add files via upload

---
+++ ayuda.py
@@ -0,0 +1,10 @@
+def ayuda():
+    print("Estos son tus comandos: \n")
+    print("'linea''coordenada x''coordenada x fin''coordenada y''coordenada y fin' te va a generar una linea")
+    print("'color list' es para ver todos los colores")
+    print("'color set' 'NOMBRE' cambio el color de la linea")
+    print("'fondo''NOMBRE'cambia el color del fondo")
+    print("'triangulo''coordenada x1''coordenada y1 fin''coordenada x2''coordenada y2''coordenada x3''coordenada y3'")
+    print("cuadrado'x'y''lado' poniendo estas coordenadas se crea el cuadrado")
+    print("circulo 'centro_x' 'centro_y' 'radio' con estas coordenadas se crea nuesto cisculo")
+    print("rectagulo 'x''y''ancho''alto'con este se crea un rectangulo")
---
+++ comandos.cmd
@@ -0,0 +1,11 @@
+ayuda
+linea = 100 200 100 500
+color list
+color set marron
+fondo rosa
+grosor 100
+triangulo = 100 100 200 200 150 300
+equilatero 800 300 800
+cuadrado 100 200 500
+circulo 100 200 50
+rectangulo 100 200 100 300
