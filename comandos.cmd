5a70d315e9c66941c2381cc55483e09d75845a50
HAlexis88 <126806311+HAlexis88@users.noreply.github.com>
2023-06-02 08:13:57 -0700

    Update comandos.cmd

--- comandos.cmd
+++ comandos.cmd
@@ -5,7 +5,8 @@ color set marron
 fondo rosa
 grosor 100
 triangulo = 100 100 200 200 150 300
-equilatero 800 300 800
-cuadrado 100 200 500
+equilatero = 100 400 400
+isosceles = 200 200 300 400
+cuadrado 100 600 500
 circulo 100 200 50
 rectangulo 100 200 100 300
